989ef5139c0f3df5277a10d55dedf889c1db350f
jcwilk <jcwilk@gmail.com>
2011-02-10 02:07:54 -0800

    Fixed javascript

--- lib/yapc/message.rb
+++ lib/yapc/message.rb
@@ -1,4 +1,5 @@
 require 'yapc/utility'
+require 'digest/sha1'
 
 module Yapc
   class Message
@@ -14,13 +15,14 @@ module Yapc
   %body
     %input{:type => 'text', :name => 'text', :id => 'text-field'}
     .messages
-      .message-start
+      #messages-start
     %script{:src => "/pusher.js"}
 EOF
     end
 
     def create
-      Pusher['messages'].trigger 'message-create', :text => params[:text]
+      addr = env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR']
+      Pusher['messages'].trigger 'message-create', :text => params[:text], :ip_digest => Digest::SHA1.hexdigest(addr+'llerkt3040f0dago-0o')[0..10]
       render params.inspect
     end
   end
--- public/pusher.js
+++ public/pusher.js
@@ -2,11 +2,13 @@ var pusher = new Pusher('d5593d81364f0abee5b0');
 var myChannel = pusher.subscribe('messages');
 
 myChannel.bind('message-create', function(thing) {
-  $("<p>"+thing.text+"</p>").insertAfter('.message-start');
+  if($('#msg-'+thing.ip_digest)[0]){
+    $('#msg-'+thing.ip_digest).text(thing.text)
+  }else{
+    $("<p id='"+'msg-'+thing.ip_digest+"'>"+thing.text+"</p>").insertAfter('#messages-start');
+  }
 });
 
 $('#text-field').keydown(function(e) {
-  if(e.keyCode == '13'){
-    $.post('/m',$('#text-field').serialize());
-  }
+  $.post('/m',$('#text-field').serialize());
 });
