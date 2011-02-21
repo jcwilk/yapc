64a5d73c680ec1c06565bf326ec9b15a9e6967b1
jcwilk <jcwilk@gmail.com>
2011-02-20 22:31:26 -0800

    Refactored the memcache code so it will fall back to the fake cache when the connection fails

--- lib/cache_manager/middleware.rb
+++ lib/cache_manager/middleware.rb
@@ -4,16 +4,30 @@ module CacheManager
       @app = app
     end
 
+    def client
+      @client ||= begin
+        if ENV['RACK_ENV'] == 'production'
+          Dalli::Client.new('localhost:11211').tap(&:stats) #stats so it'll trigger a network error if there's no memcached running
+        else
+          Faker.new
+        end
+      rescue Dalli::NetworkError
+        Faker.new
+      end
+    end
+
     def call(env)
-      env[MEMCACHE_KEY] = ENV['RACK_ENV'] == 'production' ? Dalli::Client.new('localhost:11211') : self
+      env[MEMCACHE_KEY] = client
       @app.call(env)
     end
 
-    def method_missing(*args)
-      if block_given?
-        yield
-      else
-        nil
+    class Faker
+      def method_missing(*args)
+        if block_given?
+          yield
+        else
+          nil
+        end
       end
     end
   end
